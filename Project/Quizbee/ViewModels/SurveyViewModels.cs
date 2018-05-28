d236a1e1c7fa05e2dc881ea85f002148c2c8655c
numanhaider29 <numanhaider29@yahoo.com>
2018-05-28 15:01:46 +0500

    viewmodel

---
+++ Project/Quizbee/ViewModels/SurveyViewModels.cs
@@ -0,0 +1,33 @@
+﻿using Quizbee.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Quizbee.ViewModels
+{
+    public class SurveyListViewModel : ListingBaseViewModel
+    {
+        public List<Survey> Surveys { get; set; }
+    }
+
+    public class SurveyViewModel : BaseViewModel
+    {
+        public int ID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        [Range(0, 23, ErrorMessage = "Hours should be between 0 to 23")]
+        public int Hours { get; set; }
+        [Required]
+        [Range(0, 59, ErrorMessage = "Minutes should be between 0 to 59")]
+        public int Minutes { get; set; }
+        public int NoOfQuestions { get; set; }
+
+        public List<Question> Questions { get; set; }
+        public TimeSpan TimeDuration { get; internal set; }
+    }
+}
