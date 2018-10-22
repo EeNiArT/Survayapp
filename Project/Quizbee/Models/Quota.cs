679a3f865e8fdef692fab53e00da450656df273b
EeNiArT <34338278+EeNiArT@users.noreply.github.com>
2018-10-22 18:29:17 +0500

    model added

---
+++ Project/Quizbee/Models/Quota.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Quizbee.Models
+{
+    public class Quota : BaseModel
+    {
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ByGender { get; set; }
+        public string Byage { get; set; }
+        public string ByCity { get; set; }
+        public DateTime CreatedOn { get; set; }
+        
+
+    }
+}
---
+++ Project/Quizbee/Models/SkipLogic.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Quizbee.Models
+{
+    public class SkipLogic : BaseModel
+    {
+        public virtual SQuestion S_Question { get; set; } 
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool SkipSurvey { get; set; }
+        public virtual Option Option { get; set; } 
+        public string SkipSQuestionsIDs { get; set; }
+        public DateTime CreatedOn { get; set; }
+        
+    }
+}
